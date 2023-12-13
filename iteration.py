ec1e6c57277fa3ee0e7b11d50dc812cd24939e3c
Heberth Deza <heberth2788@gmail.com>
2023-12-12 22:04:37 -0500

    day 3 v2

--- iteration.py
+++ iteration.py
@@ -1,11 +1,36 @@
-# For sentence
+# for sentence
 listA = [1, 2, 3, 4, 5]
 print("Start the bucle 'for'")
 for aux in listA:
-    print("Aux = " + str(aux))
+    print("Aux =", aux) # print("Aux = " + str(aux))
 else:
     print("'else' sentence and end of bucle 'for'")
-print("End of 'for' and 'else' sentences")
+print("End of 'for' and 'else' sentences\n")
 
-# While sentence
+# while sentence
+num = 0
+while num <= 5:
+    print(num, "HD") # print(str(num) + "HD")
+    num += 1
+else:
+    print("'eslse' sentence and end of bucle 'while'\n")
+
+# break sentence
+num = 0
+while num <= 100:
+    print(num, "HD") # print(str(num) + "HD")
+    num += 1
+    if num == 10:
+        break # break doesn't allow that else execution
+else:
+    print("Else of while\n")
+
+for aux in listA:
+    print("Aux =", aux)
+    if aux == 3:
+        break # break doesn't allow that else execution
+else:
+    print("Else of for\n")
+print("Kernel panic")
+
 
